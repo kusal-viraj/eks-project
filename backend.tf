d939133786d89a613c957417c35b5b754550f978
viraj Ariyasinghe <viraj.a@papertrl.com>
2024-12-06 16:01:07 +0530

    project update

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "papertrl-terraform"
-    key    = "dev-env/teraform.tfstate"
-    region = "us-east-2"
-    #dynamodb_table = "terraform-state-lock"
-    encrypt = true
-  }
-}
